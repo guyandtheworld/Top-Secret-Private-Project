ddcd31ad56205773e68c0179af9f2334d73e9bd1
Adarsh S <4darshofficial@gmail.com>
2018-08-27 11:32:00 +0530

    Optimise the initial algorithm.
    One edit away (Modification) : Use a flag bit to keep track of the first difference. If the flag bit has been set as True, when another difference is encountered then return
    False since it's the second difference.
    
    One insert away: The first argument is the largest string and the second argument is the smaller one. If a character difference is encountered (THE FIRST TIME), increment the second variable. If a character diff is encountered for the second time, check if indexes are equal. If they aren't equal, then that means it is the second character difference
    then return False.

--- CTCI/Arrays/ctci_5.py
+++ CTCI/Arrays/ctci_5.py
@@ -5,29 +5,38 @@ one edit (or zero edits) away.
 """
 
 class Solution:
+    def check_modify_helper(self, str1, str2):
+        diff = False
+        for i in range(len(str1)):
+            if str1[i] != str2[i]:
+                if diff:
+                    return False
+                else:
+                    diff = True
+        return diff
+
+    def check_edit_helper(self, str1, str2):
+        i = j = 0
+        while i < len(str1) and j < len(str2):
+            if str1[i] != str2[j]:
+                if i != j:
+                    return False
+                else:
+                    j+=1
+            else:
+                i += 1; j += 1
+        return True
+
     def check_edit(self, str1, str2):
         strlen1 = len(str1)
         strlen2 = len(str2)
-        edit_bit = []
         if strlen1 == strlen2:
-            for i in range(strlen1):
-                if str1[i] != str2[i]:
-                    edit_bit.append(0)
-        elif strlen1 > strlen2:
-            for i in range(strlen2):
-                if str1[i] != str2[i]:
-                    edit_bit.append(0)
-                    break
-            if str1[strlen1 - 1] != str2[strlen2 - 1]:
-                edit_bit.append(0)
-        else:
-            for i in range(strlen1):
-                if str1[i] != str2[i]:
-                    edit_bit.append(0)
-                    break
-            if str1[strlen1 - 1] != str2[strlen2 - 1]:
-                edit_bit.append(0)
-        return len(edit_bit) == 1
+            return self.check_modify_helper(str1, str2)
+        elif strlen1 + 1 == strlen2:
+            return self.check_edit_helper(str1, str2)
+        elif strlen1 - 1 == strlen2:
+            return self.check_edit_helper(str2, str1)
+        return False
 
 
     # Too cluttered. Split code to functions
@@ -35,4 +44,4 @@ class Solution:
     # Using while loop, you could get rid of the edge end condition
 
 sol = Solution()
-print(sol.check_edit("pale", "pale"))
+print(sol.check_edit("pale", "pals"))
