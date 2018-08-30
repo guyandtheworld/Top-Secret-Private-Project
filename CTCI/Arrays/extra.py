9123c60b6def8de0e9fd724aca7b18b4a2347c4a
Adarsh S <4darshofficial@gmail.com>
2018-08-30 16:14:51 +0530

    Shorten string algorithm
    * Has a complexity of O(n).
    * We use two data structures here, a list and a dictionary.
    * List is used to store the order of letters appearing on the string.
    * Using the index of the list items as the key for the dictionary, we store the number of occurrence of simultaneous same letters.
    
    Recommended Algorithm
    
    * Use a list data structure with O(n) complexity
    * Go through string. If string at i-1 is different from string at i. Reset the counter to 0 and append the char + the count of char to the list
    * The last character is never added, so add the char with the last value of the counter.

---
+++ CTCI/Arrays/extra.py
@@ -0,0 +1,8 @@
+"""
+A good exercise to practice strings, arrays, and general data structures is to implement your own version of
+StringBuilder, HashTable and Array List.
+"""
+
+"""
+From next lesson onwards write the CTCI algorithm on the notebook because you'll have to whiteboard for the interview
+"""
