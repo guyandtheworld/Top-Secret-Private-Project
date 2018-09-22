2d38e8d7c1c542f0b6a80aaa3ffdfc06d33cfe47
Adarsh S <4darshofficial@gmail.com>
2018-09-22 21:12:29 +0530

    Palindrome of a string
    Method 1: Adarsh's recursion.
    
    You start from the beginning and you recurse till the end all the
    while passing the start pointer till the end. Then on
    your way back, you increment the start pointer and you compare that
    value with the value of the recursion we're on from the end.
    
    Method 2: Runner method
    
    This is much easier to understand IMO. You have two pointers, a slow and
    fast. The fast ptr moves twice as fast so that by the end the fast
    pointer reaches the end the slow pointer would be half way through the
    list. You should've pushed the values of the stack pointer to a stack
    
    Then you compare the rest of the slow pointer till the end and pop from
    the stack.

---
+++ CTCI/LinkedList/ctci_6.py
@@ -0,0 +1,46 @@
+"""
+Palindrome: Implement a function to check if a linked list is a palindrome.
+"""
+
+import unittest
+
+from CTCI.LinkedList.LinkedList import LinkedList
+
+
+def is_palindrome(runner_beg, runner_end):
+    if not runner_end.next:
+        return runner_beg.next, runner_beg.value == runner_end.value
+    start, flag = is_palindrome(runner_beg, runner_end.next)
+    if flag:
+        return start.next, start.value == runner_end.value
+    return start.next, flag
+
+
+def is_palindrome_v2(ll):
+    fast = slow = ll.head
+
+    stack = []
+
+    while fast and fast.next:
+        stack.append(slow.value)
+        fast = fast.next.next
+        slow = slow.next
+
+    while slow:
+        if slow.value != stack.pop():
+            return False
+        slow = slow.next
+
+    return True
+
+class Test(unittest.TestCase):
+
+    def test_linked_list(self):
+        ll = LinkedList()
+        ll.add_multiple(['m', 'a', 'l', 'l', 'a', 'm'])
+        print(ll)
+        print(is_palindrome(ll.head, ll.head))
+        print(is_palindrome_v2(ll))
+
+if __name__ == "__main__":
+    unittest.main()
