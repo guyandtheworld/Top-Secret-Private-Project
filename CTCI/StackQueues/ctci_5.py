12f0c97496474168546dd09519bc91489b45037b
Adarsh S <4darshofficial@gmail.com>
2018-10-19 20:24:48 +0530

    Brute force approach by Adarsh
    So you have two stacks you can use to sort.
    So basically the mStack stores all the final result.
    The first iteration finds the biggest element from the stack and takes
    it to the bottom. Then the stack traverses
    till the second last element and finds the biggest one from there.
    So one by one it brings the largest item to the botton, and finally we're
    left with a sorted stack with the smallest item on top

---
+++ CTCI/StackQueues/ctci_5.py
@@ -0,0 +1,84 @@
+"""
+    Sort Stack: Write a program to sort a stack such that the smallest items are on the top. You can use
+    an additional temporary stack, but you may not copy the elements into any other data structure
+    (such as an array). The stack supports the following operations: push, pop, peek, and isEmpty. 
+"""
+
+
+import unittest
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+class Stack:
+    def __init__(self):
+        self.stack = None
+        self.top = 0
+
+    def push(self, value):
+        node = Node(value)
+        node.next = self.stack
+        self.stack = node
+        self.top += 1
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        popped = self.stack
+        self.stack = self.stack.next
+        self.top -= 1
+        return popped.value
+
+    def is_empty(self):
+        if not self.stack:
+            return True
+        return False
+
+    def view(self):
+        ptr = self.stack
+        while ptr:
+            print(ptr.value, end=" | ")
+            ptr = ptr.next
+        print()
+
+
+class Test(unittest.TestCase):
+
+    def sort(self):
+        top = self.mStack.top
+        for i in range(top):
+            print("Iter : ", i+1)
+            tmax = self.mStack.pop()
+            for j in range(top-1):
+                temp = self.mStack.pop()
+                if temp > tmax:
+                    self.tStack.push(tmax)
+                    tmax = temp
+                else:
+                    self.tStack.push(temp)
+            self.mStack.push(tmax)
+            for j in range(top-1):
+                self.mStack.push(self.tStack.pop())
+            top -= 1
+            self.mStack.view()
+            print()
+
+    def test_stack_min(self):
+        self.mStack = Stack()
+        self.tStack = Stack()
+
+        self.mStack.push(1)
+        self.mStack.push(9)
+        self.mStack.push(3)
+        self.mStack.push(8)
+        self.mStack.push(6)
+
+        self.sort()
+
+
+if __name__ == "__main__":
+    unittest.main()
