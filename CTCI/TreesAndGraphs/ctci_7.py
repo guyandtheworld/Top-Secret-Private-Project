2d7b273e495f33e13464a028de83b5b26a11c557
Adarsh S <4darshofficial@gmail.com>
2018-11-17 11:38:19 +0530

    Building projects in dependency based order. This algorithm currently doesn't support loops to detect whether to prove interdependency of the projects. The optimum technique should've been to like build a graph from the given data and choose the node which has no incoming node and add it to the build order. Then delete all the outgoing edge from that particular node. Then recursively check the rest of the nodes for dependecy-less nodes and repeat this process and all nodes which doesn't have incoming nodes to the build order and then deleting it. If it gets to a point where there are nodes remaining we can't find nodes without incoming edges, it means that there is no valid build order.

--- CTCI/TreesAndGraphs/ctci_7.py
+++ CTCI/TreesAndGraphs/ctci_7.py
@@ -1,9 +1,8 @@
 """
 Build Order: You are given a list of projects and a list of dependencies (which is a list of pairs of projects,
-where the second project is dependent on the first project). All of a project's dependencies must be built 
-before the project is. Find a build order that will allow the projects to be built. If there is no valid 
-build order, return an error. EXAMPLE Input: projects: a, b, c, d, e, f dependencies: (a, d), (f, b), (b, d), (f, a), (d, c) 
-Output: f, e, a, b, d, c 
+where the second project is dependent on the first project). All of a project's dependencies must be built
+before the project is. Find a build order that will allow the projects to be built. If there is no valid
+build order, return an error.
 """
 
 import unittest
@@ -14,6 +13,59 @@ class Node:
         self.name = name
         self.visited = False
         self.children = []
+
+    def __repr__(self):
+        return str(self.name)
+
+
+def BFS(root):
+    queue = []
+    root.visited = True
+    queue.append(root)
+
+    while len(queue) != 0:
+        node = queue.pop(0)
+        print(node, end='->')
+        for n in node.children:
+            if not n.visited:
+                n.visited = True
+                queue.append(n)
+
+
+def create(node, depends):
+    if node not in depends:
+        return
+
+    for child in depends[node]:
+        if child not in node.children:
+            node.children.append(child)
+            create(child, depends)
+
+
+def create_build(projects, dependencies):
+    depends = {}
+    non_dependent_nodes = []
+    for p in projects:
+        flag = False
+        for d in dependencies:
+            if d[1] == p:
+                flag = True
+                break
+        if not flag:
+            non_dependent_nodes.append(p)
+
+    if not len(non_dependent_nodes):
+        raise("No valid build order")
+
+    for d in dependencies:
+        if d[0] in depends:
+            depends[d[0]].append(d[1])
+        else:
+            depends[d[0]] = [d[1]]
+
+    for head in non_dependent_nodes:
+        create(head, depends)
+        BFS(head)
 
 
 class Test(unittest.TestCase):
@@ -26,9 +78,14 @@ class Test(unittest.TestCase):
         d = Node('d')
         e = Node('e')
         f = Node('f')
+        g = Node('g')
 
-        project = [a, b, c, d, e, f]
-        dependencies = [()]
+        project = [a, b, c, d, e, f, g]
+        dependencies = [(f, a), (f, b), (f, c), (c, a),
+                        (b, a), (a, e), (b, e), (d, g)]
+        # dependencies = [(a, d), (f, b), (b, d), (f, a), (d, c), (d, e), (c, f)]
+
+        create_build(project, dependencies)
 
 
 if __name__ == '__main__':
